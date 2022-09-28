474172bf34cce439dcd46c113ab6a16f42ce799b
WesleySquire <Wesley.Squire@keyin.com>
2022-09-28 13:28:13 -0230

    Commit2

--- src/main/java/com/keyin/qap1/QAPExample.java
+++ src/main/java/com/keyin/qap1/QAPExample.java
@@ -4,4 +4,8 @@ public class QAPExample {
     public boolean findSequenceInWord(String wordToFind, String charSequence) {
     return charSequence.contains(wordToFind);
     }
+
+    public boolean doesEqualTen(int firstNumber, int numToAdd) {
+        return ((firstNumber + numToAdd) == 10);
+    }
 }
--- src/test/java/com/keyin/qap1/QAPExampleTest.java
+++ src/test/java/com/keyin/qap1/QAPExampleTest.java
@@ -7,15 +7,22 @@ public class QAPExampleTest {
 
     @Test
     public void QAPExampleTest1(){
-    QAPExample qapExample = new QAPExample();
+        QAPExample qapExample = new QAPExample();
 
-    Assertions.assertTrue(qapExample.findSequenceInWord("find", "is_find_in_this_char_sequence"));
-    Assertions.assertFalse(qapExample.findSequenceInWord("find_me", "is_find_in_this_char_sequence"));
+        Assertions.assertTrue(qapExample.findSequenceInWord("find", "is_find_in_this_char_sequence"));
+        Assertions.assertFalse(qapExample.findSequenceInWord("find_me", "is_find_in_this_char_sequence"));
+        Assertions.assertNotNull(qapExample.findSequenceInWord("find_me", "is_find_in_this_char_sequence"));
 
     }
 
     @Test
     public void QAPExampleTest2(){
+        QAPExample qapExample = new QAPExample();
+
+        Assertions.assertTrue(qapExample.doesEqualTen(7,3));
+        Assertions.assertFalse(qapExample.doesEqualTen(7,7));
+
+
 
     }
     @Test
