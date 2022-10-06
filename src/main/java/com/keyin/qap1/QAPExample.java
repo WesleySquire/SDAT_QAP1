cf771fa918ae9092542212d098b992acba674934
WesleySquire <Wesley.Squire@keyin.com>
2022-10-06 12:48:48 -0230

    FinalCommit

--- src/main/java/com/keyin/qap1/QAPExample.java
+++ src/main/java/com/keyin/qap1/QAPExample.java
@@ -2,11 +2,26 @@ package com.keyin.qap1;
 
 public class QAPExample {
     public boolean findSequenceInWord(String wordToFind, String charSequence) {
-    return charSequence.contains(wordToFind);
+        return charSequence.contains(wordToFind);
     }
 
     public boolean doesEqual(int firstNumber, int numToAdd, int numToEqual) {
         return ((firstNumber + numToAdd) == numToEqual);
+    }
+
+    public String russianRoulette(int chamber){
+
+        switch (chamber) {
+            case 2 -> {
+                System.out.println("BANG, You're Dead");
+                return ("BANG");
+            }
+            case 1, 3, 4, 5, 6 -> {
+                System.out.println("Click, You're Alive");
+                return ("Click");
+            }
+        }
+        return ("Please Enter Number 1-6");
     }
 
 
--- src/test/java/com/keyin/qap1/QAPExampleTest.java
+++ src/test/java/com/keyin/qap1/QAPExampleTest.java
@@ -10,9 +10,11 @@ public class QAPExampleTest {
         QAPExample qapExample = new QAPExample();
 
         Assertions.assertTrue(qapExample.findSequenceInWord("find", "is_find_in_this_char_sequence"));
+        // Ensures return statement is true that "find" is in the charSequence
         Assertions.assertFalse(qapExample.findSequenceInWord("find_me", "is_find_in_this_char_sequence"));
+        // Ensures return statement is false that "find_me" is NOT in the charSequence
         Assertions.assertNotEquals(false, qapExample.findSequenceInWord("find_me", "find_me_in_your_dreams"));
-
+        // Ensures return statement does NOT equal false so if the test is successful "find_me" is in the charSequence
     }
 
     @Test
@@ -20,18 +22,21 @@ public class QAPExampleTest {
         QAPExample qapExample = new QAPExample();
 
         Assertions.assertTrue(qapExample.doesEqual(7,3, 10));
+        // Ensures return statement is true that 7 + 3 = 10
         Assertions.assertFalse(qapExample.doesEqual(7,7, 10));
+        // Ensures return statement is false because 7 + 7 != 10
         Assertions.assertSame(qapExample.doesEqual(40,5, 45), true);
-
-
-
-
+        // Ensures return statement is the same as true. (40 + 5 = 45)
     }
     @Test
     public void QAPExampleTest3(){
         QAPExample qapExample = new QAPExample();
-
-
+        Assertions.assertEquals("BANG", qapExample.russianRoulette(2));
+        // Ensures return statement is String ("BANG")
+        Assertions.assertEquals("Click", qapExample.russianRoulette(5));
+        // Ensures return statement is String ("Click")
+        Assertions.assertNotEquals("BANG", qapExample.russianRoulette(1) );
+        // Ensures return statement != String ("BANG")
     }
 
 }
